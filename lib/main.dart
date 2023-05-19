528aadbe3182c841533881cd6b9ab84873cd678f
SakshiRathod13 <sakshirathod84352@gmail.com>
2023-05-19 14:26:20 +0530

    ch1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "sakshi",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Sakshi Rathod"),
-          backgroundColor: Colors.blue,
-        ),
-      )));
+  runApp(MaterialApp(title: "sakshi", home: homePage()));
+}
+
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sakshi Rathod"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: Text("Hello"),
+      ),
+    );
+  }
 }
