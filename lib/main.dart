f0686438a61a5a7837c4240bdcabc728d023f264
SakshiRathod13 <sakshirathod84352@gmail.com>
2023-05-19 14:11:34 +0530

    first

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "sakshi",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Sakshi Rathod"),
+          backgroundColor: Colors.blue,
+        ),
+      )));
+}
